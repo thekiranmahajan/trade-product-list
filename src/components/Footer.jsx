b05216bed09006d0f68c73e5ad65e2286f929269
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-08 14:42:24 +0530

    feat: create and style header and footer of page also grid the cards

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,19 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <span>© {currentYear} Made with ❤️ by </span>
+      <a
+        href="https://github.com/thekiranmahajan/trade-product-list"
+        target="_blank"
+        rel="noopener noreferrer"
+        className=""
+      >
+        &nbsp; Kiran Mahajan
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,15 @@
+import cartLogo from "../assets/shopping-cart.png";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav>
+      <img src={cartLogo} alt="cart-logo" />
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Cart</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Header;
