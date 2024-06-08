5b11b0f10065888ebade8eeb930372df2b2379f7
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-08 09:14:40 +0530

    feat: create a custom-hook to fetch API end point data for abstracting the logic from component + separate constants like URLs/APIs

---
+++ src/hooks/useProducts.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { FAKE_PRODUCT_API } from "../utils/constant";
+
+const useProducts = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(FAKE_PRODUCT_API);
+      const data = response.json();
+      console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  return products;
+};
+
+export default useProducts;
---
+++ src/utils/constant.js
@@ -0,0 +1,1 @@
+export const FAKE_PRODUCT_API = "https://fakestoreapi.com/products";
