089d34327ea3e5af7dcd73e7e0a240e97cc4a528
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-09 17:47:57 +0530

    feat: implement the hamburger menu and make Header complete responsive over various devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,27 @@
-import "../styles/header.css";
-
+import { useState } from "react";
 import cartLogo from "../assets/shopping-cart.png";
+import NavLinks from "./NavLinks";
+import "../styles/header.css";
+import hamburgerImg from "../assets/hamburger.svg";
+
 const Header = () => {
+  const [isHamburger, setIsHamburger] = useState(false);
+  console.log(isHamburger);
+
   return (
-    <nav>
+    <nav className="nav-container">
       <img src={cartLogo} alt="cart-logo" />
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Cart</li>
-      </ul>
+      <img
+        id="hamburger-img"
+        onClick={() => {
+          setIsHamburger((prev) => !prev);
+        }}
+        src={hamburgerImg}
+        alt="hamburger img"
+      />
+      {isHamburger && <NavLinks style="hamburger" />}
+
+      <NavLinks style="not-hamburger" />
     </nav>
   );
 };
---
+++ src/components/NavLinks.jsx
@@ -0,0 +1,12 @@
+const NavLinks = ({ style }) => {
+  return (
+    <ul className={style}>
+      <li>Home</li>
+      <li>About</li>
+      <li>Cart</li>
+      <li>Contact</li>
+    </ul>
+  );
+};
+
+export default NavLinks;
