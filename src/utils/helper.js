250e52206e514d63624dca8bb04bcb8fbb4b9e3f
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-09 19:53:45 +0530

    feat: create possible sorting functions at basic level of logic not tested yet

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -5,3 +5,21 @@ export const userQuery = (products, setFilteredProducts, searchText) => {
   setFilteredProducts(filteredData.length === 0 ? null : filteredData);
   console.log(filteredData);
 };
+
+export const priceSort = (products, setFilteredProducts) => {
+  const sortedData = products.sort((a, b) => a.price - b.price);
+  setFilteredProducts(sortedData);
+  console.log(sortedData);
+};
+export const ratingSort = (products, setFilteredProducts) => {
+  const sortedData = products.sort((a, b) => a.rating.rate - b.rating.rate);
+  setFilteredProducts(sortedData);
+  console.log(sortedData);
+};
+export const nameSort = (products, setFilteredProducts) => {
+  const sortedData = products.sort((a, b) =>
+    a.name.localCompare(b.rating.rate)
+  );
+  setFilteredProducts(sortedData);
+  console.log(sortedData);
+};
