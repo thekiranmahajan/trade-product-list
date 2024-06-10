d62aa1bdcb5ae3ed73e267c858d3e6bd3c0029ca
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-10 13:57:48 +0530

    feat: add trycatch for better error handlig and minimizing edge cases also add isLoading state to easy shimmer effect

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -4,18 +4,27 @@ import { FAKE_PRODUCT_API } from "../utils/constant";
 const useProducts = () => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
+  const fetchProducts = async () => {
+    setIsLoading(true);
+    try {
       const response = await fetch(FAKE_PRODUCT_API);
       const data = await response.json();
       setProducts(data);
       setFilteredProducts(data);
-    };
+    } catch (error) {
+      console.log("Failed to load products..Please try again later.", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
     fetchProducts();
   }, []);
 
-  return { products, filteredProducts, setFilteredProducts };
+  return { products, filteredProducts, setFilteredProducts, isLoading };
 };
 
 export default useProducts;
