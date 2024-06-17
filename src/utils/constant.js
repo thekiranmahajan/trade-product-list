01e48f79f8b136d4591d1675f0a297a8c84e62b5
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-17 22:19:32 +0530

    feat: abstracting the constants in separate file for reusability + avoiding use of ENV variables for simplicity for now

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,23 @@
 export const FAKE_PRODUCT_API = "https://fakestoreapi.com/products";
+
+export const FEATURED_USERS_API =
+  "https://prod.imkloud.com/api/user-groups/v2/retrieve/featured/5G46npPukBgjkHEWw";
+
+export const USERS_GROUPS_API =
+  "https://prod.imkloud.com/api/user-groups/v2/retrieve/5G46npPukBgjkHEWw";
+
+export const API_OPTIONS = {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    auth: {
+      apiKey: "3AxFH2MfurQYFS8LRFCHpdHFHrjKhp",
+      orgId: "YshrFJcsDP8PT6cjr",
+      type: "apiKey",
+    },
+    pagination: {
+      limit: 10,
+      skip: 0,
+    },
+  }),
+};
