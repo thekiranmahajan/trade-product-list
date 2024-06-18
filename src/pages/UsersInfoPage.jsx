72709d9427dfe9366de39b599ff96bfb6fc036cd
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-06-18 07:50:28 +0530

    feat: create a user card to show data using a better UI

--- src/pages/UsersInfoPage.jsx
+++ src/pages/UsersInfoPage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import useUsersInfo from "../hooks/useUsersInfo";
+import { UserCard } from "../components";
 
 const UsersInfoPage = () => {
   const { featuredUsers, userGroups, isLoading } = useUsersInfo();
   console.log(featuredUsers);
-  console.log(userGroups);
+  // console.log(userGroups);
 
-  return <div></div>;
+  return (
+    <div className="card-container">
+      {featuredUsers && <UserCard featuredUsers={featuredUsers[0]} />}
+    </div>
+  );
 };
 
 export default UsersInfoPage;
